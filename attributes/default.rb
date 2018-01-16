ebaaca1a7039c49367e4ab3204aa781ff640122c
Flavio-PC <craig@amtdev.com>
2018-01-16 00:06:07 +0000

    added template stubs and initial attributes

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['amti_php']['memory_limit'] = "128M"
+default['amti_php']['post_max_size'] = "8M"
+default['amti_php']['upload_max_filesize'] = "2M"
+default['amti_php']['max_execution_time'] = "30"
+default['amti_php']['max_input_time'] = "60"
+default['amti_php']['debugger_allow_hosts'] = "127.0.0.1"
+default['amti_php']['max_input_vars'] = "1000"
+default['amti_php']['curl.cainfo'] = "null"
