f1f47c2dd08203621a7e0a9fc8fb91a267a98679
AMTdev <craig@amtdev.com>
2019-12-24 02:32:06 +0000

    removed delivery dir

--- .delivery/build_cookbook/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'build_cookbook'
-maintainer 'Advanced Marketing Training, Inc.'
-maintainer_email 'craig@amtdev.com'
-license 'apachev2'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
-depends 'delivery-truck'
